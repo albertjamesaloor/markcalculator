24a474b61b6c9942b05eb3642df47e7de68892a2
albertjamesaloor <161922319+albertjamesaloor@users.noreply.github.com>
2024-09-05 05:49:00 +0530

    Update menu_hider.py

--- menu_hider.py
+++ menu_hider.py
@@ -1,6 +1,7 @@
-hide_menu_style =
+hide_menu_style = """
   <style>
   #MainMenu {visibility : hidden; }
   footer {visibility : hidden; }
   </style>
+  """
 st.markdown(hide_menu_style. unsafe_allow_html=True)
