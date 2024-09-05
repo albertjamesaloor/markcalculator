9486b95521cb44bfa604550ea686e9778bd69415
albertjamesaloor <161922319+albertjamesaloor@users.noreply.github.com>
2024-09-05 05:53:50 +0530

    Update page_config.py

--- page_config.py
+++ page_config.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from PIL import image
+from PIL import Image
 
 img = Image.open('logo.jpg')
 
-st.set_page_config(page_title="Marks Calculator', page_icon=img)
+st.set_page_config(page_title="Marks Calculator", page_icon=img)
