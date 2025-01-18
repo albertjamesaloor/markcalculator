00aa7f8b7e51b6356f18e0ae61e862de531edda7
albertjamesaloor <161922319+albertjamesaloor@users.noreply.github.com>
2025-01-18 09:49:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-# Create a form to enter marks
+# Import the Streamlit library
+import streamlit as st
+# Define a list of subjects
+subjects = ["Physics", "Maths", "Chemistry", "Biology", "Hindi", "Computer Science","English"]
+# Create a dropdown menu for each subject
+selected_subject = st.selectbox('Select a subject', subjects)
+# Display the selected subject back to the user
+st.write("The subject you selected is:", selected_subject)
+# Create a slider
+percentage = st.slider('Percentage:', 33, 100)
+st.write(f'You selected: {percentage}%')
+# Define the main function
+def main():
+  # Create a form to enter marks
   marks = st.form("enter_marks")
   pt1_input = marks.number_input("Enter PT1 mark:", min_value=0, max_value=40, value=0)
   hf_input = marks.number_input("Enter HF mark:", min_value=0, max_value=80, value=0)
   pt2_input = marks.number_input("Enter PT2 mark:", min_value=0, max_value=40, value=0)
-
   # Add a submit button to the form
   submitted = marks.form_submit_button("Submit")
-
   # Display the entered marks if the form is submitted
   total_marks=0
   EMH=80*percentage/100
@@ -49,20 +60,7 @@
                 st.write("You need to score ",round(marks_to_score),"marks in Session Ending Exam to cross",percentage, "%")
             else:
               st.write("Sorry... it is not possible, realise your past mistakes and don't let them repeat.\n Strive for a lesser score")
-
-
-
-
-
-# Define a list of subjects
-subjects = ["Physics", "Maths", "Chemistry", "Biology", "Hindi", "Computer Science","English"]
-
-# Create a dropdown menu for each subject
-selected_subject = st.selectbox('Select a subject', subjects)
-# Display the selected subject back to the user
-st.write("The subject you selected is:", selected_subject)
-
-# Create a slider
-percentage = st.slider('Percentage:', 33, 100)
-
-st.write(f'You selected: {percentage}%')
+# Call the main function
+if __name__ == "__main__":
+  main()
+st.write("Note: percentage weightage used is PT1 - 10%, HF - 30%, PT2 - 10%, SEE - 50%")
